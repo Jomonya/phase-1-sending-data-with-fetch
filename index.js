e28a24d7db4a1ff2b17e8370be0927c25e91b39e
Joy <joynandako1@gmail.com>
2024-04-08 23:07:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+  const url = 'http://localhost:3000/users';
+  const data = JSON.stringify({ name, email });
+
+  const fetchRequest = fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: data
+  })
+  .then(response => response.json())
+  .then(data => {
+    const userId = document.createElement('p');
+    userId.textContent = `New user ID: ${data.id}`;
+    document.body.appendChild(userId);
+  })
+  .catch(error => {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorMessage);
+  });
+
+  return fetchRequest;
+}
+
+
+  
